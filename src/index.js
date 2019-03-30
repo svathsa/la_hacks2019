10a3e9b7254629151b137af1bd8b739171ab3aa0
svathsa <svathsa@g.ucla.edu>
2019-03-30 03:51:34 -0700

    figuring out firebase stuff

--- src/index.js
+++ src/index.js
@@ -7,10 +7,12 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/rootReducer';
+//The code below is used for firebase
+import Firebase, { FirebaseContext } from './components/Firebase';
 
 const store = createStore(rootReducer);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<FirebaseContext.Provider value={new Firebase()}><Provider store={store}><App /></Provider></FirebaseContext.Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
