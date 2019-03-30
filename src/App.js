ed6612e93dd26dc4a310c2c06fe973f900d9931e
neerajsamtani <neerajjsamtani@gmail.com>
2019-03-30 00:47:57 -0700

    connect with redux.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
 // import dummy components
-// import Navbar from './components/Navbar'
-// import Home from './components/Home'
-// import About from './components/About'
-// import Contact from './components/Contact'
-// import Post from './components/Post'
+import Login from './components/Login'
+import Home from './components/Home'
 // Will be used to implement Redux
-// import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
-      /* < BrowserRouter>
+       < BrowserRouter>
         <div className="App">
-          <Navbar />
           <Switch>
-          <Route exact path='/' component={Home} ></Route>
-          <Route path='/about' component={About} ></Route>
-          <Route path='/contact' component={Contact} ></Route>
-          <Route path='/:post_id' component={Post} ></Route>
+          <Route exact path='/' component={Login} ></Route>
+          <Route exact path='/home' component={Home} ></Route>
           </Switch>
         </div>
-      </BrowserRouter> */
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      </BrowserRouter>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 // The code below will be used for redux
-// import { createStore } from 'redux';
-// import { Provider } from 'react-redux';
-// import rootReducer from './reducers/rootReducer';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/rootReducer';
 
-// const store = createStore(rootReducer);
+const store = createStore(rootReducer);
 
-// ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
