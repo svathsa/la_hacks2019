753de8998f59979ce02e22506ae543ac2d91e575
Nikil Roashan Selvam <nikilrselvam@gmail.com>
2019-03-30 07:26:45 -0700

    Updated Login.js with Appropriate Styling

--- src/components/Login.js
+++ src/components/Login.js
@@ -63,21 +63,16 @@ class Login extends Component {
     }
     render() {
         return(
-            <div class="login">
-                <div class="d-flex justify-content-center h-100">
-                <div class="user_card">
-                    <div class="d-flex justify-content-center">
-                        <div class="brand_logo_container">
-                            <img src={logo} class="brand_logo" alt="Logo" />
+            <div className="login">
+                <div className="d-flex justify-content-center h-100">
+                <div className="user_card">
+                    <div className="d-flex justify-content-center">
+                        <div className="brand_logo_container">
+                            <img src={logo} className="brand_logo" alt="Logo" />
                         </div>
                     </div>
-                    <div class="d-flex justify-content-center mt-3 login_container">
-                        <button type="button" name="button" class="btn login_btn">Login</button>
-                    </div>
-                    <div class="mt-4">
-                        <div class="d-flex justify-content-center links">
-                            Don't have an account? <a href="#" class="ml-2">Sign Up</a>
-                        </div>
+                    <div className="d-flex justify-content-center mt-3 login_container">
+                        <button type="button" name="button" className="btn login_btn" onClick={()=>this.authenticate()}>Login</button>
                     </div>
                 </div>
             </div>
