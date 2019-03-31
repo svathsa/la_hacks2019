b77e3e0a895b8f49edfe358869481307a646c487
neerajsamtani <neerajjsamtani@gmail.com>
2019-03-30 19:07:27 -0700

    Working on HomePage 1.0

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
 import Navbar from './Navbar'
+import './Home.css'
 
 class Home extends Component {
     render() {
         return(
-            <div class="home">
+            <div className="home">
                 <Navbar />
-                <h4 className="center">Home</h4>
+                <div className = "homepage">
+                    <p id = "myteams">
+                        My Teams
+                    </p>
+                    <div className="buttonwrapper">
+                        <a className="teamNameButton" id = "teamButtonText" href = "#">Team name 1</a>
+                        <a className="teamNameButton" id = "teamButtonText"  href = "#">Team name 2</a>
+                        <a className="teamNameButton" id = "teamButtonText" href = "#">Team name 2</a>
+                        <a className="teamNameButton" id = "teamButtonText" href = "#">Team name 3</a>
+                        <button type="button" className="teamButton" id="teamButtonText" data-toggle="modal" data-target="#exampleModalCenter">
+                            + Add Team
+                        </button>
+                    </div>
+                    <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                        <div className="modal-dialog modal-dialog-centered" role="document">
+                            <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalCenterTitle">Team Name</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div className="modal-body">
+                            <form>
+                                <div className="form-group">
+                                <input type="text" className="form-control" id="message-name" />
+                                </div>
+                                <button type="button" class="btn btn-primary" data-dismiss="modal">Create Team</button>
+                            </form>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                </div>
             </div>
         )
     }
