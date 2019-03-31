c088048bd0c62bd0d6d4703e4984d53b57c4eea8
Nikil Roashan Selvam <nikilrselvam@gmail.com>
2019-03-30 19:32:18 -0700

    Added EditProfile without compound members

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import LeadPage from './components/LeadPage'
 import Team from './components/Team'
 import ProfilePage from './components/ProfilePage'
 import BigProfilePage from './components/BigProfilePage'
+import BigEditProfile from './components/BigEditProfile'
 // Will be used to implement Redux
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
@@ -21,6 +22,7 @@ class App extends Component {
           <Route exact path='/leadpage' component={LeadPage} ></Route>
           <Route exact path='/team/:team_id' component={Team} ></Route>
           <Route exact path='/profile/:profile_id' component={BigProfilePage} ></Route>
+          <Route exact path='/editprofile/:profile_id' component={BigEditProfile} ></Route>
           </Switch>
         </div>
       </BrowserRouter>
---
+++ src/components/EditProfile.js
@@ -0,0 +1,192 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar'
+import firebase from 'firebase';
+import base, {firebaseApp} from '../components/Firebase/firebase'
+import './EditProfile.css';
+
+function mapObject(object, callback) {
+    if(object != null){
+    return Object.keys(object).map(function (key) {
+      return callback(key, object[key]);
+    });
+    }else{
+        
+    }
+  }
+
+
+
+class EditProfile extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            profile: null
+        }
+    
+        this.handleChangeName = this.handleChangeName.bind(this);
+        this.handleChangeEmail = this.handleChangeEmail.bind(this);
+        this.handleChangeAge = this.handleChangeAge.bind(this);
+        this.handleChangeEducation = this.handleChangeEducation.bind(this);
+        this.handleChangeRole = this.handleChangeRole.bind(this);
+        this.handleChangeCity = this.handleChangeCity.bind(this);
+        this.handleChangeZip = this.handleChangeZip.bind(this);
+        this.handleChangeRating = this.handleChangeRating.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+
+      handleChangeName(event) {
+        this.setState({
+            profile: {
+              ...this.state.profile,
+              name: event.target.value
+            }
+        });
+    }
+    handleChangeEmail(event) {
+        this.setState({
+            profile: {
+              ...this.state.profile,
+              email: event.target.value
+            }
+        });
+    }
+    handleChangeAge(event) {
+        this.setState({
+            profile: {
+              ...this.state.profile,
+              age: event.target.value
+            }
+        });
+    }
+    handleChangeEducation(event) {
+        this.setState({
+            profile: {
+              ...this.state.profile,
+              education: event.target.value
+            }
+        });
+    }
+    handleChangeRole(event) {
+        this.setState({
+            profile: {
+              ...this.state.profile,
+              role: event.target.value
+            }
+        });
+    }
+    handleChangeCity(event) {
+        this.setState({
+            profile: {
+              ...this.state.profile,
+              city: event.target.value
+            }
+        });
+    }
+    handleChangeZip(event) {
+        this.setState({
+            profile: {
+              ...this.state.profile,
+              zip: event.target.value
+            }
+        });
+    }
+    handleChangeRating(event) {
+        this.setState({
+            profile: {
+              ...this.state.profile,
+              rating: event.target.value
+            }
+        });
+    }
+    
+      handleSubmit(event) {
+        alert('Your profile has been updated successfully!');
+        event.preventDefault();
+
+        var updates = {};
+        var userId = firebase.auth().currentUser.uid;
+        updates['/Users/' + userId] = this.state.profile;
+        firebase.database().ref().update(updates);
+      }
+
+    componentWillMount() {
+        
+        var user = firebase.database().ref('Users/' + this.props.id);
+        user.once('value', function(snapshot) {
+            console.log(snapshot.val());
+            
+            
+        }).then((snapshot) => {
+            this.setState({
+                profile: snapshot.val()
+            })
+            
+        })
+        
+        // API Call to find data using the id above 
+        // Store result of query in a variable called user
+        // DONT REMOVE COMMENTED CODE BELOW
+        // this.setState({
+        //     profile: {
+        //         first_name: 'Neeraj',
+        //         last_name: 'Samtani',
+        //         age: 18
+        //     }
+        // })
+        // axios.get('https://jsonplaceholder.typicode.com/posts/'+id)
+        //     .then(res => {
+        //         this.setState({
+        //             post: res.data
+        //         })
+        //     })
+    }
+
+    render() {
+            const post = this.state.profile ? (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" value={this.state.profile.name} onChange={this.handleChangeName} />
+                </label>
+                <label>
+                    Email:
+                    <input type="text" value={this.state.profile.email} onChange={this.handleChangeEmail} />
+                </label>
+                <label>
+                    Age:
+                    <input type="text" value={this.state.profile.age} onChange={this.handleChangeAge} />
+                </label>
+                <label>
+                    Education:
+                    <input type="text" value={this.state.profile.education} onChange={this.handleChangeEducation} />
+                </label>
+                <label>
+                    Role:
+                    <input type="text" value={this.state.profile.role} onChange={this.handleChangeRole} />
+                </label>
+                <label>
+                    City:
+                    <input type="text" value={this.state.profile.city} onChange={this.handleChangeCity} />
+                </label>
+                <label>
+                    Zip:
+                    <input type="text" value={this.state.profile.zip} onChange={this.handleChangeZip} />
+                </label>
+                <label>
+                    Rating:
+                    <input type="text" value={this.state.profile.rating} onChange={this.handleChangeRating} />
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
+            ):(
+                <div className="center">Loading ...</div>
+            )
+            return (
+                <div>
+                    {post}
+                </div>
+            )
+    }
+}
+
+export default EditProfile
