6e2ec3a36e173437e99665103dc762b14bef816a
svathsa <svathsa@g.ucla.edu>
2019-03-31 03:49:09 -0700

    lead page works sorta

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ProfilePage from './components/ProfilePage'
 import BigProfilePage from './components/BigProfilePage'
 import RoleCard from './components/RoleCard'
 import BigEditProfile from './components/BigEditProfile'
+import BigLeadPage from './components/BigLeadPage'
 // Will be used to implement Redux
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Navbar from './components/Navbar';
@@ -24,7 +25,7 @@ class App extends Component {
           <Route exact path='/leadpage' component={LeadPage} ></Route>
           <Route exact path='/teampage/:team_id' component={TeamPage} ></Route>
           <Route exact path='/profile/:profile_id' component={BigProfilePage} ></Route>
-          <Route exact path='/leadPage' component={LeadPage}></Route>
+          <Route exact path='/leadPage/:team_id' component={BigLeadPage}></Route>
           <Route exact path='/rolecard' component={RoleCard}></Route>
           <Route exact path='/editprofile/:profile_id' component={BigEditProfile} ></Route>
           </Switch>
