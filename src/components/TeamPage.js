ccf56b39f3f4abfa754492dfd08c7227217cef60
Nikil Roashan Selvam <nikilrselvam@gmail.com>
2019-03-31 03:18:22 -0700

    Update TeamPage.js

--- src/components/TeamPage.js
+++ src/components/TeamPage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Navbar from './Navbar'
 import './TeamPage.css'
 import TeamMember from './TeamMember';
+import firebase from 'firebase';
+import base, {firebaseApp} from '../components/Firebase/firebase'
 
 function mapObject(object, callback) {
     if(object != null){
@@ -15,32 +17,91 @@ function mapObject(object, callback) {
   
 
 class TeamPage extends Component {
+
+    
+
+    constructor(props){
+        super(props);
+        this.state= {
+            ids: []
+        }
+
+        this.fetchData =this.fetchData.bind(this);
+    }
+
+    componentDidMount(){
+        this.fetchData();
+    }
+
+    fetchData(){
+        var refer=this;
+        let id = this.props.match.params.team_id;
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                console.log("Reached!");
+                firebase.database().ref('Teams/'+ id+'/roles').once('value').then(async(snapshot)=> {
+                    let teamResult= await snapshot.val();
+                    console.log("TeamResult");
+                    console.log(teamResult);
+                    mapObject(teamResult, function (key, value) {
+                        mapObject(value, function (dummy, userID) {
+                            console.log("UID "+userID);
+                            //add id to state
+                            refer.setState({
+                                ids: [...refer.state.ids, userID]
+                            });
+                            console.log("State");
+                            console.log(refer.state);
+                            
+                        })
+                             
+                    })
+                });
+            } else {
+              // No user is signed in.
+            }
+          })
+
+          firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                console.log("Reached!");
+                firebase.database().ref('Teams/'+ id + '/name').once('value').then(async(snapshot)=> {
+                    let tempName= await snapshot.val();
+                    refer.setState({
+                        ids: refer.state.ids,
+                        name: tempName
+                    });
+                    console.log("State");
+                    console.log(refer.state);
+                });
+            } else {
+              // No user is signed in.
+            }
+          })
+    }
+
     render() {
         let id = this.props.match.params.team_id;
         console.log(id);
         return(
             <div class="team">
                 < Navbar />
-                <h4 className="center">TEAM {id}</h4>
                 <div class = "teamheader">
-                    <h1>team name</h1>  
+                    <h1>{this.state.name}</h1>  
                 </div>
             
             <div class = "status">
-                <p>Insert team status here
+                <p>Finalized
                 </p>
             </div>
 
                 <ul class="cards">
-                <li class="cards__item">
-                    <TeamMember id="oQU21vFIVYh98iajfZ2qeGkUskF3"/> {/*each member*/}
-                </li>
-                <li class="cards__item">
-                    <TeamMember id="3XVYwVpuIcVjcm2u8Fejrp1AHXo1"/> {/*each member*/}
-                </li>
-                <li class="cards__item">
-                    <TeamMember id="6X3oxHb4hKXCRxwlIvQbpKOXKjr2"/> {/*each member*/}
-                </li>
+                
+
+                {console.log("State")}
+                {console.log(this.state)}
+                {this.state.ids.map((userid, i)=> 
+                <li class="cards__item" key={i}> <TeamMember id={userid}/> </li>)}
                 </ul>
             </div>
         )
