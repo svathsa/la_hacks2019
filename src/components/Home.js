80a45b120a5ec32f82e62ee36818b952f7065320
Nikil Roashan Selvam <nikilrselvam@gmail.com>
2019-03-31 00:24:43 -0700

    Home.js Works!!

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,20 +5,62 @@ import firebase from 'firebase'
 import AddTeam from './AddTeam'
 import './Home.css'
 
-class Home extends Component {
-    state = {
-        teams: []
-    }
-    componentDidMount(){
+function mapObject(object, callback) {
+    if(object != null){
+    return Object.keys(object).map(function (key) {
+      return callback(key, object[key]);
+    });
+    }else{
         
     }
+  }
+
+class Home extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            teams: []
+        };
+        this.addTeam = this.addTeam.bind(this)
+
+    }
+
+    
     addTeam = (team) => {
-    team.id = Math.random();
+        
+    if(!this.state.teams.includes(team)){
     let teams = [...this.state.teams, team];
     this.setState({
         teams
     });
     }
+    }
+
+    componentDidMount(){
+        const refer= this;
+        firebase.auth().onAuthStateChanged(function(user) {
+            if (user) {
+                firebase.database().ref('Teams/').once('value').then(async(snapshot)=> {
+                    console.log(snapshot.val());
+                    let teamResult= await snapshot.val();
+                    mapObject(teamResult, function (key, value) {
+                        console.log(value);
+                        console.log("UID "+user.uid);
+                        console.log("Lead "+value.lead);
+
+                        if(value.lead==user.uid){
+                            refer.addTeam(value);
+                        }
+                             
+                    })
+                });
+            } else {
+              // No user is signed in.
+            }
+          });
+    }
+
     render() {
             return(
             <div className="home">
